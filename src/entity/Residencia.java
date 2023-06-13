47be0f5ad328ed7f67174c027746cda070e66281
Lucas Davi <lucasdavi99@hotmail.com>
2023-06-13 11:49:06 -0300

    1° commit

---
+++ src/entity/Residencia.java
@@ -0,0 +1,60 @@
+package entity;
+
+public class Residencia {
+	private int id;
+    private String endereco; //numero do apt e bloco
+    private boolean disponibilidade;
+    
+    public Residencia(String endereco, boolean disponibilidade, int id) {
+        this.endereco = endereco;
+        this.disponibilidade = disponibilidade;
+        this.id = id;
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public boolean isDisponibilidade() {
+		return disponibilidade;
+	}
+
+	public void setDisponibilidade(boolean disponibilidade) {
+		this.disponibilidade = disponibilidade;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+    
+    
+
+    
+}
+
